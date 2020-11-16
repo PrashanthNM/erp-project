395cb47593c1963bd63b20fa768ce7c8b79b8ee1
PrashanthNM <54975166+PrashanthNM@users.noreply.github.com>
2020-11-16 22:29:21 +0530

    Update urls.py

--- miniproject/urls.py
+++ miniproject/urls.py
@@ -20,7 +20,6 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('',include('erp.urls')),
     path('accounts/',include('accounts.urls'))]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
