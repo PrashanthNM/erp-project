3a6af4057e5611805b2fdc157bceec70efdf6bb7
PrashanthNM <54975166+PrashanthNM@users.noreply.github.com>
2020-11-16 21:29:09 +0530

    Add files via upload

--- miniproject/settings.py
+++ miniproject/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,6 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT=os.path.join(BASE_DIR,'STATICFILES')
+
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
